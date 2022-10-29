a10a60c7cdc9acff1f5c3de7299f89dea438378e
Tom Newby <me@tomnewby.net>
2022-10-29 11:52:23 +1000

    Nit pick

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,24 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: [
+          'Roboto',
+          'Inter',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
       colors: {
         primary: {
           25: '#F2F5FF',
